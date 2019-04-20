f0d35496840dd02e4894d2640680670ba94c297e
Wyzzus <wyzzus@gmail.com>
2019-04-21 02:41:15 +0300

    Add Animal Generator

---
+++ Assets/Scripts/AnimalGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BodyStruct
+{
+    public Transform HeadSpot;
+    public Transform TailSpot;
+    public GameObject BodySpot;
+}
+
+public class AnimalGenerator : MonoBehaviour {
+
+    public Color color;
+
+    public BodyStruct[] Bodies;
+
+    public BodyStruct CurrentBody;
+
+    public GameObject[] Heads;
+    public GameObject[] Tails;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        CurrentBody = Bodies[Random.Range(0, Bodies.Length)];
+        CurrentBody.BodySpot.SetActive(true);
+        Instantiate<GameObject>(Heads[Random.Range(0, Heads.Length)], CurrentBody.HeadSpot);
+        Instantiate<GameObject>(Tails[Random.Range(0, Tails.Length)], CurrentBody.TailSpot);
+
+        color = Random.ColorHSV();
+
+        foreach(SpriteRenderer sr in GetComponentsInChildren<SpriteRenderer>())
+        {
+            sr.color = color;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
